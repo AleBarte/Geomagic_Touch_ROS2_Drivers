df99f7eb882351de2a458d2e1fb784549ccdb70b
AleBarte <alexbartesaghi@gmail.com>
2026-01-19 10:52:13 +0100

    Completed porting of omni_common to ros2. Now porting omni_msgs

---
+++ omni_common/include/omni_common/omni_state.hpp
@@ -0,0 +1,90 @@
+#ifndef OMNI_STATE_HPP_
+#define OMNI_STATE_HPP_
+
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/twist_stamped.hpp"
+#include "geometry_msgs/msg/wrench_stamped.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
+#include "joy_msgs/msg/joy.hpp"
+
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+#include <unistd.h>
+#include <sstream>
+#include <iostream>
+
+#include <HL/hl.h>
+#include <HD/hd.h>
+#include <HDU/hduError.h>
+#include <HDU/hduVector.h>
+#include <HDU/hduMatrix.h>
+#include <HDU/hduQuaternion.h>
+#include "eigen3/Eigen/Dense"
+#include "eigen3/Eigen/Geometry"
+
+#include "omni_msgs/msg/omni_button_event.hpp"
+#include "omni_msgs/msg/omni_feedback.hpp"
+#include "omni_msgs/msg/omni_state.hpp"
+
+struct OmniState {
+  hduVector3Dd position;  //3x1 vector of position
+  hduVector3Dd velocity;  //3x1 vector of velocity
+  hduVector3Dd inp_vel1;  //3x1 history of velocity used for filtering velocity estimate
+  hduVector3Dd inp_vel2;
+  hduVector3Dd inp_vel3;
+  hduVector3Dd out_vel1;
+  hduVector3Dd out_vel2;
+  hduVector3Dd out_vel3;
+  hduVector3Dd pos_hist1; //3x1 history of position used for 2nd order backward difference estimate of velocity
+  hduVector3Dd pos_hist2;
+  hduQuaternion rot;
+  hduVector3Dd joints;
+  hduVector3Dd force;   //3 element double vector force[0], force[1], force[2]
+  float thetas[7];
+  int buttons[2];
+  int buttons_prev[2];
+  bool lock;
+  bool close_gripper;
+  hduVector3Dd lock_pos;
+  double units_ratio;
+};
+
+class geoRos : public rclcpp::Node
+{
+
+public:
+
+    geoRos(const std::string &node_name);
+    void init(OmniState* s);
+
+private:
+
+
+    void forceCallback(const omni_msgs::msg::OmniFeedback::SharedPtr msg);
+    void publish();
+
+    rclcpp::Publisher<omni_msgs::msg::OmniState>::SharedPtr state_publisher_;
+    rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pose_publisher_;
+    rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr twist_publisher_;
+    rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_publisher_;
+    rclcpp::Publisher<joy_msgs::msg::Joy>::SharedPtr button_publisher_;
+    rclcpp::Publisher<omni_msgs::msg::OmniButtonEvent>::SharedPtr button_event_publisher_;
+    rclcpp::Subscription<omni_msgs::msg::OmniFeedback>::SharedPtr haptic_sub_;
+    OmniState* state_;
+
+    // Parameters
+    std::string ref_frame_;
+    std::string units_;
+    double publishing_rate_;
+
+    // Flags
+    bool is_initialized_ = false;
+
+    // Common timer object
+    rclcpp::TimerBase::SharedPtr timer_;
+
+};
+#endif  // OMNI_STATE_HPP_
---
+++ omni_common/src/omni_state_main.cpp
@@ -0,0 +1,174 @@
+//TODO: Change way angular an linear velocities are taken from the device. The API gives ways of retrieving them. Don't know why they were not used.
+
+#include "../../omni_common/include/omni_common/omni_state.hpp"
+
+int calibrationStyle;
+
+HDCallbackCode HDCALLBACK omni_state_callback(void *pUserData)
+{
+  OmniState *omni_state = static_cast<OmniState *>(pUserData);
+  if (hdCheckCalibration() == HD_CALIBRATION_NEEDS_UPDATE) {
+      std::cout << "Updating Calibration.." << std::endl;
+      hdUpdateCalibration(calibrationStyle);
+  }
+  hdBeginFrame(hdGetCurrentDevice());
+  // Get transform and angles
+  hduMatrix transform;
+  hdGetDoublev(HD_CURRENT_TRANSFORM, transform);
+  hdGetDoublev(HD_CURRENT_JOINT_ANGLES, omni_state->joints);
+  hduVector3Dd gimbal_angles;
+  hdGetDoublev(HD_CURRENT_GIMBAL_ANGLES, gimbal_angles);
+  // Notice that we are inverting the Z-position value and changing Y <---> Z
+  // Position
+  omni_state->position = hduVector3Dd(transform[3][0], -transform[3][2], transform[3][1]);
+  omni_state->position /= omni_state->units_ratio;
+  // Orientation (quaternion)
+  hduMatrix rotation(transform);
+  rotation.getRotationMatrix(rotation);
+  hduMatrix rotation_offset( 0.0, -1.0, 0.0, 0.0,
+                             1.0,  0.0, 0.0, 0.0,
+                             0.0,  0.0, 1.0, 0.0,
+                             0.0,  0.0, 0.0, 1.0);
+  rotation_offset.getRotationMatrix(rotation_offset);
+  omni_state->rot = hduQuaternion(rotation_offset * rotation);
+  // Velocity estimation
+  hduVector3Dd vel_buff(0, 0, 0);
+  vel_buff = (omni_state->position * 3 - 4 * omni_state->pos_hist1
+      + omni_state->pos_hist2) / 0.002;  //(units)/s, 2nd order backward dif
+  omni_state->velocity = (.2196 * (vel_buff + omni_state->inp_vel3)
+      + .6588 * (omni_state->inp_vel1 + omni_state->inp_vel2)) / 1000.0
+      - (-2.7488 * omni_state->out_vel1 + 2.5282 * omni_state->out_vel2
+          - 0.7776 * omni_state->out_vel3);  //cutoff freq of 20 Hz
+  omni_state->pos_hist2 = omni_state->pos_hist1;
+  omni_state->pos_hist1 = omni_state->position;
+  omni_state->inp_vel3 = omni_state->inp_vel2;
+  omni_state->inp_vel2 = omni_state->inp_vel1;
+  omni_state->inp_vel1 = vel_buff;
+  omni_state->out_vel3 = omni_state->out_vel2;
+  omni_state->out_vel2 = omni_state->out_vel1;
+  omni_state->out_vel1 = omni_state->velocity;
+
+  //~ // Set forces if locked
+  //~ if (omni_state->lock == true) {
+    //~ omni_state->force = 0.04 * omni_state->units_ratio * (omni_state->lock_pos - omni_state->position)
+        //~ - 0.001 * omni_state->velocity;
+  //~ }
+  hduVector3Dd feedback;
+  // Notice that we are changing Y <---> Z and inverting the Z-force_feedback
+  feedback[0] = omni_state->force[0];
+  feedback[1] = omni_state->force[2];
+  feedback[2] = -omni_state->force[1];
+  hdSetDoublev(HD_CURRENT_FORCE, feedback);
+
+  //Get buttons
+  int nButtons = 0;
+  hdGetIntegerv(HD_CURRENT_BUTTONS, &nButtons);
+  omni_state->buttons[0] = (nButtons & HD_DEVICE_BUTTON_1) ? 1 : 0;
+  omni_state->buttons[1] = (nButtons & HD_DEVICE_BUTTON_2) ? 1 : 0;
+
+  hdEndFrame(hdGetCurrentDevice());
+
+  HDErrorInfo error;
+  if (HD_DEVICE_ERROR(error = hdGetError())) {
+    hduPrintError(stderr, &error, "Error during main scheduler callback");
+    if (hduIsSchedulerError(&error))
+      return HD_CALLBACK_DONE;
+  }
+
+  float t[7] = { 0., omni_state->joints[0], omni_state->joints[1],
+      omni_state->joints[2] - omni_state->joints[1], gimbal_angles[0],
+      gimbal_angles[1], gimbal_angles[2] };
+  for (int i = 0; i < 7; i++)
+    omni_state->thetas[i] = t[i];
+  return HD_CALLBACK_CONTINUE;
+}
+
+/*******************************************************************************
+ Automatic Calibration of Phantom Device - No character inputs
+ *******************************************************************************/
+void HHD_Auto_Calibration() {
+  int supportedCalibrationStyles;
+  HDErrorInfo error;
+
+  hdGetIntegerv(HD_CALIBRATION_STYLE, &supportedCalibrationStyles);
+  if (supportedCalibrationStyles & HD_CALIBRATION_ENCODER_RESET) {
+    calibrationStyle = HD_CALIBRATION_ENCODER_RESET;
+    std::cout << "HD_CALIBRATION_ENCODER_RESET.." << std::endl;
+  }
+  if (supportedCalibrationStyles & HD_CALIBRATION_INKWELL) {
+    calibrationStyle = HD_CALIBRATION_INKWELL;
+    std::cout << "HD_CALIBRATION_INKWELL.." << std::endl;
+  }
+  if (supportedCalibrationStyles & HD_CALIBRATION_AUTO) {
+    calibrationStyle = HD_CALIBRATION_AUTO;
+    std::cout << "HD_CALIBRATION_AUTO.." << std::endl;
+  }
+  if (calibrationStyle == HD_CALIBRATION_ENCODER_RESET) {
+    do {
+      hdUpdateCalibration(calibrationStyle);
+      std::cout << "Calibrating.. (put stylus in well)" << std::endl;
+      if (HD_DEVICE_ERROR(error = hdGetError())) {
+        hduPrintError(stderr, &error, "Reset encoders reset failed.");
+        break;
+      }
+    } while (hdCheckCalibration() != HD_CALIBRATION_OK);
+    std::cout << "Calibration complete." << std::endl;
+  }
+  while(hdCheckCalibration() != HD_CALIBRATION_OK) {
+    usleep(1e6);
+    if (hdCheckCalibration() == HD_CALIBRATION_NEEDS_MANUAL_INPUT)
+      std::cout << "Calibration needs manual input.." << std::endl;
+    else if (hdCheckCalibration() == HD_CALIBRATION_NEEDS_UPDATE) {
+      std::cout << "Calibration updated successfully" << std::endl;
+      hdUpdateCalibration(calibrationStyle);
+    }
+    else
+      std::cerr << "Unknown calibration status.." << std::endl;
+  }
+}
+
+int main(int argc, char** argv)
+{
+    rclcpp::init(argc, argv);
+    
+    // Init the Geomagic Device
+    HDErrorInfo error;
+    HHD hHD = hdInitDevice(HD_DEFAULT_DEVICE);
+    if (HD_DEVICE_ERROR(error = hdGetError())) {
+        hduPrintError(stderr, &error, "Failed to initialize device");
+        rclcpp::shutdown();
+        return -1;
+    }
+
+    hdEnable(HD_FORCE_OUTPUT);
+
+    // Start the scheduler
+    hdStartScheduler();
+    if (HD_DEVICE_ERROR(error = hdGetError())) {
+        hduPrintError(stderr, &error, "Failed to start the scheduler");
+        hdDisableDevice(hHD);
+        rclcpp::shutdown();
+        return -1;
+    }
+
+    // Automatic Calibration
+    HHD_Auto_Calibration();
+
+    // Ros Node
+    OmniState state;
+    auto geo_ros_node = std::make_shared<geoRos>("geo_ros_node");
+    geo_ros_node->init(&state);
+    hdScheduleAsynchronous(
+        omni_state_callback, &state, HD_MAX_SCHEDULER_PRIORITY);
+
+    rclcpp::executors::MultiThreadedExecutor executor;
+    
+    executor.add_node(geo_ros_node);
+    executor.spin();
+
+    // On shutdown
+    hdStopScheduler();
+    hdDisableDevice(hHD);
+    rclcpp::shutdown();
+    return 0;
+}
